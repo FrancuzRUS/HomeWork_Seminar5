720845cc40e7b0651991440215000c3089fc209b
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-05-29 23:02:33 +0300

    Task1 Compelted

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,28 @@
 ﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
 
+Console.WriteLine("Введите коэфиценты для первой прямой: ");
 
+Console.Write("k1 =  ");
+int k1 = int.Parse(Console.ReadLine());
+Console.Write("b1 = : ");
+int b1 = int.Parse(Console.ReadLine());
 
+Console.WriteLine("Введите коэфиценты для второй прямой: ");
+Console.Write("k2 = ");
+int k2 = int.Parse(Console.ReadLine());
+Console.Write("b2 = ");
+int b2 = int.Parse(Console.ReadLine());
 
+int x = 0; 
+int y = 0;
+
+if (k1 == k2)
+Console.WriteLine($"Прямые параллельны и не пересекаются!");
+
+else 
+{
+x = (b2 - b1)/(k1 - k2);
+y = k1*x+b1;
+Console.WriteLine($"Точка пересечения двух прямых имеет координаты x ={x} , y ={y}");
+}
 
