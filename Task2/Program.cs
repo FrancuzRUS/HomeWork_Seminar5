5e9852fd34c98b3caa5e254b985a1c7950f0e965
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-05-30 09:26:08 +0300

    task2 Completed

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,5 +5,24 @@
 // В результате показать координаты, которые получатся.
 // при k = 2 получаем "(0,0) (4,0) (4,4) (0,4)"
 
+double [,] array = new double[4,2];
+int i = 0 ;
+int j = 0 ;
+double [,] MASS = new double[4,2];
+Console.WriteLine("Введите коэффициент масштабирования: ");
+double k= double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты вершин: ");
 
+for (i = 0; i<4; i++)
+    for (j = 0; j<2; j++)
+       { array[i,j] = double.Parse(Console.ReadLine()); }
 
+for (i = 0; i<4; i++)
+    for (j = 0; j<2; j++)
+        MASS[i,j]=k*array[i,j];
+
+for (i = 0; i<4; i++)
+{ Console.WriteLine("Координаты точки ");
+    for (j = 0; j<2; j++)
+       { Console.WriteLine($" {MASS[i,j]}");}
+}
